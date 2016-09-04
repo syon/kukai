29ed43253e58f5d468df9f3cd6af84cdf9580d10
syon <syon.xiii@gmail.com>
2016-09-04 16:19:25 +0900

    multiple fonts

--- babel/my/Container.js
+++ babel/my/Container.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 import axios from 'axios';
 import $ from 'jquery';
 import HtmlToReact from 'html-to-react';
@@ -15,20 +16,20 @@ class Container extends React.Component {
     this.drawSvg = this.drawSvg.bind(this);
 
     this.state = {
-      g: '<div></div>',
+      list: [],
     };
   }
 
   componentDidMount() {
     this.drawSvg('Oradano-mincho-GSRR', 'Book', 'normal', 'normal', 'Oradano明朝フォント');
-    // this.drawSvg('Koku Mincho Regular', 'Regular', 'normal', 'normal', '刻明朝フォント');
-    // this.drawSvg('02UtsukushiMincho', 'Regular', 'normal', 'normal', 'うつくし明朝体');
-    // this.drawSvg('HarenosoraMincho', 'Regular', 'normal', 'normal', 'はれのそら明朝');
-    // this.drawSvg('HannariMincho', 'Regular', 'normal', 'normal', 'はんなり明朝');
-    // this.drawSvg('Honoka Mincho', 'Regular', 'normal', 'normal', 'ほのか明朝');
-    // this.drawSvg('Dejima', 'mincho', 'normal', 'normal', '出島明朝フォント');
-    // this.drawSvg('Aozora Mincho', 'Regular', 'normal', 'normal', 'あおぞら明朝 Regular');
-    // this.drawSvg('Aozora Mincho', 'Bold', 'normal', 'bold', 'あおぞら明朝 Bold');
+    this.drawSvg('Koku Mincho Regular', 'Regular', 'normal', 'normal', '刻明朝フォント');
+    this.drawSvg('02UtsukushiMincho', 'Regular', 'normal', 'normal', 'うつくし明朝体');
+    this.drawSvg('HarenosoraMincho', 'Regular', 'normal', 'normal', 'はれのそら明朝');
+    this.drawSvg('HannariMincho', 'Regular', 'normal', 'normal', 'はんなり明朝');
+    this.drawSvg('Honoka Mincho', 'Regular', 'normal', 'normal', 'ほのか明朝');
+    this.drawSvg('Dejima', 'mincho', 'normal', 'normal', '出島明朝フォント');
+    this.drawSvg('Aozora Mincho', 'Regular', 'normal', 'normal', 'あおぞら明朝 Regular');
+    this.drawSvg('Aozora Mincho', 'Bold', 'normal', 'bold', 'あおぞら明朝 Bold');
   }
 
   drawSvg(tn, ts, fs, fw, txt) {
@@ -41,7 +42,9 @@ class Container extends React.Component {
     } })
       .then((res) => {
         const g = $(res.data).find('g');
-        this.setState({ g: g.prop('outerHTML') });
+        const list = this.state.list;
+        list.push({ key: tn + ts, g: g.prop('outerHTML') });
+        this.setState({ list });
       })
       .catch((res) => {
         console.error(res);
@@ -49,17 +52,30 @@ class Container extends React.Component {
   }
 
   render() {
-    let svgInner = '';
-    if (this.state.g) {
-      svgInner = parser.parse(this.state.g);
-    }
+    const nodes = [];
+    _.each(this.state.list, (l) => {
+      let svgInner = '';
+      if (l.g) {
+        svgInner = parser.parse(l.g);
+      }
+      nodes.push(
+        <svg
+          key={l.key}
+          x="0px"
+          y="0px"
+          width="400"
+          height="60"
+          viewBox="0 0 400 40"
+        >
+          {svgInner}
+        </svg>
+      );
+    });
     return (
       <div>
         <h1>Container</h1>
         <h5>width: {this.props.width} px</h5>
-        <svg x="0px" y="0px" width="400" height="60" viewBox="0 0 400 40">
-          {svgInner}
-        </svg>
+        {nodes}
       </div>
     );
   }
