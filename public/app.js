b75a05cac59913672d69080ac076813febd3adf8
syon <syon.xiii@gmail.com>
2016-09-04 11:36:47 +0900

    add sample fonts

--- public/app.js
+++ public/app.js
@@ -1,11 +1,14 @@
 $(function() {
 
-  drawSvg('Oradano-mincho-GSRR', 'Book', 'normal', 'normal', '日本語のフォント');
-  drawSvg('Koku Mincho Regular', 'Regular', 'normal', 'normal', '日本語のフォント');
-  drawSvg('02UtsukushiMincho', 'Regular', 'normal', 'normal', '日本語のフォント');
-  drawSvg('HarenosoraMincho', 'Regular', 'normal', 'normal', '日本語のフォント');
-  drawSvg('HannariMincho', 'Regular', 'normal', 'normal', '日本語のフォント');
-  drawSvg('Honoka Mincho', 'Regular', 'normal', 'normal', '日本語のフォント');
+  drawSvg('Oradano-mincho-GSRR', 'Book', 'normal', 'normal', 'Oradano明朝フォント');
+  drawSvg('Koku Mincho Regular', 'Regular', 'normal', 'normal', '刻明朝フォント');
+  drawSvg('02UtsukushiMincho', 'Regular', 'normal', 'normal', 'うつくし明朝体');
+  drawSvg('HarenosoraMincho', 'Regular', 'normal', 'normal', 'はれのそら明朝');
+  drawSvg('HannariMincho', 'Regular', 'normal', 'normal', 'はんなり明朝');
+  drawSvg('Honoka Mincho', 'Regular', 'normal', 'normal', 'ほのか明朝');
+  drawSvg('Dejima', 'mincho', 'normal', 'normal', '出島明朝フォント');
+  drawSvg('Aozora Mincho', 'Regular', 'normal', 'normal', 'あおぞら明朝 Regular');
+  drawSvg('Aozora Mincho', 'Bold', 'normal', 'bold', 'あおぞら明朝 Bold');
 
 });
 
@@ -15,8 +18,8 @@ function drawSvg(tn, ts, fs, fw, txt) {
     data: {
       ttfName: tn,
       ttfStyle: ts,
-      fontStyle: fs,
-      fontWeight: fw,
+      fontStyle: fs, // italic, oblique
+      fontWeight: fw, // 100 - 900, bold, etc.
       textBody: txt
     }
   }).done(function(svg) {
